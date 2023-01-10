84a229b0b688c619bcae35d8e76d1ad88ece69c8
buhahenil <75998671+buhahenil@users.noreply.github.com>
2023-01-10 18:17:26 +0530

    ad

--- BillSystem/Customer.asmx.cs
+++ BillSystem/Customer.asmx.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Script.Serialization;
 
 namespace BillSystem
 {
@@ -14,13 +18,26 @@ namespace BillSystem
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
+   
     public class Customer : System.Web.Services.WebService
     {
+        public string connectionString = ConfigurationManager.ConnectionStrings["BillSystem"].ConnectionString;
 
         [WebMethod]
-        public string 
+        
+        public string GetCustomer(string Customerid)
         {
-            
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("getRecord", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Customerid", Customerid);
+            DataTable dt = new DataTable();
+            con.Open();
+            sda.Fill(dt);
+            con.Close();
+            return JsonConvert.SerializeObject(dt);
         }
     }
 }
--- BillSystem/WebForm1.aspx
+++ BillSystem/WebForm1.aspx
@@ -9,19 +9,31 @@
 <body>
     <form id="form1" runat="server">
         <%--<input type="button" id="btnAddnew" value="Add New" style="background-color: MediumSlateBlue; color: white" />--%>
+        
         <input type="search" id="searchBox" name="searchbox" placeholder="Search Box">
 
-
-        
-        
     </form>   
 </body>
     <script src="Scripts/jquery-3.6.3.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
-            $("#searchBox").select(function () {
-                var selectrecord = {};
-                selectrecord.
+            $("#searchBox").on("keyup", function () {
+                var value = $(this).val();
+                value
+                $ajax({
+                    url: "/WebForm1.aspx/getRecord",
+                    type: "POST",
+                    datatype: "json",
+                    ContentType: "application/json; charset=utf-8",
+                    data: JSON.stringify(data),
+                    success: function (data) {
+                        console.log();
+                    },
+                    error: function (err) {
+                        //debugger;
+                        console.log(err);
+                    }
+                });
             });
         });
     </script>
